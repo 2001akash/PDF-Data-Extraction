e36ea974a6c6add4dd4bb3df9257615a8de8539a
2001akash <2001akashdeep@gmail.com>
2024-02-25 23:15:49 +0530

    second commit

--- pdf_extractor/pdf_extractor/urls.py
+++ pdf_extractor/pdf_extractor/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# pdf_extractor/urls.py
+#h
 
 # pdf_extractor/urls.py
 from django.contrib import admin
