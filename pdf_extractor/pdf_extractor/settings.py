e508c23e32dd34ea7b7e8840b12d38eebd5c3cd3
2001akash <2001akashdeep@gmail.com>
2024-03-11 23:10:53 +0530

    first Commit

--- pdf_extractor/pdf_extractor/settings.py
+++ pdf_extractor/pdf_extractor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-i=3np!0ovyph1vut(%g+qxc^=!b(iab53wx(hx@k0sz%sv51jr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -122,3 +122,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
